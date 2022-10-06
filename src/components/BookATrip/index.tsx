ba61190a5969ff60bbf3c0d9862fdd72456fa532
Md. Imriaz Uddin <imriaz.uddin@bjitgroup.com>
2022-10-06 09:21:49 +0600

    Book a Trip section start

--- src/components/BookATrip/index.tsx
+++ src/components/BookATrip/index.tsx
@@ -1,25 +1,51 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
+import DestinationIcon from "../../Assets/images/destination.png";
+import PaymentIcon from "../../Assets/images/payment.png";
+import CarIcon from "../../Assets/images/car.png";
 
 const BookATrip = () => {
-    return (
-      <div className="Booking">
-        <div className="Booking__LeftSide">
-          <h1 className="Booking__LeftSide__SectionTopHeading">
-            Easy and Fast
-          </h1>
-          <h2 className="Booking__LeftSide__SectionHeading">
-            Book your next trip in 3 easy steps
-          </h2>
-          <div className="Booking__LeftSide__Feature"></div>
-        </div>
-        <div className="Booking__RightSide">
-          <div className="Booking__RightSide__TripCard">
-            <h1>HEllo</h1>
+  return (
+    <div className="Booking">
+      <div className="Booking__LeftSide">
+        <h1 className="Booking__LeftSide__SectionTopHeading">Easy and Fast</h1>
+        <h2 className="Booking__LeftSide__SectionHeading">
+          Book your next trip in 3 easy steps
+        </h2>
+        <div className="Booking__LeftSide__Feature">
+          <div className="Booking__LeftSide__Feature__Card">
+            <img src={DestinationIcon} alt="Features icon" />
+            <div className="Booking__LeftSide__Feature__Card__Body">
+              <h3 className="Booking__LeftSide__Feature__Card__Body__Title">
+                Choose Destination
+              </h3>
+              <p className="Booking__LeftSide__Feature__Card__Body__Description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna,
+                tortor tempus.
+              </p>
+            </div>
+          </div>
+          <div className="Booking__LeftSide__Feature__Card">
+            <img src={PaymentIcon} alt="Features icon" />
+            <div className="Booking__LeftSide__Feature__Card__Body">
+              <h3 className="Booking__LeftSide__Feature__Card__Body__Title">
+                Make Payment
+              </h3>
+              <p className="Booking__LeftSide__Feature__Card__Body__Description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna,
+                tortor tempus.
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    );
+      <div className="Booking__RightSide">
+        <div className="Booking__RightSide__TripCard">
+          <h1>HEllo</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BookATrip;
