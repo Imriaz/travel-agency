3e6c14be74e46422eaadd2737944f4d0dae745a0
Md. Imriaz Uddin <imriaz.uddin@bjitgroup.com>
2022-10-06 12:17:11 +0600

    Book a Trip section Left side complete

--- src/components/BookATrip/index.tsx
+++ src/components/BookATrip/index.tsx
@@ -3,6 +3,10 @@ import "./style.css";
 import DestinationIcon from "../../Assets/images/destination.png";
 import PaymentIcon from "../../Assets/images/payment.png";
 import CarIcon from "../../Assets/images/car.png";
+import TripCartImage from "../../Assets/images/Trip cart image.jpg";
+import Leaf from "../../Assets/images/Leaf.png";
+import Map from "../../Assets/images/map icon.png";
+import Send from "../../Assets/images/send.png";
 
 const BookATrip = () => {
   return (
@@ -37,11 +41,37 @@ const BookATrip = () => {
               </p>
             </div>
           </div>
+          <div className="Booking__LeftSide__Feature__Card">
+            <img src={CarIcon} alt="Features icon" />
+            <div className="Booking__LeftSide__Feature__Card__Body">
+              <h3 className="Booking__LeftSide__Feature__Card__Body__Title">
+                Reach Airport on Selected Date
+              </h3>
+              <p className="Booking__LeftSide__Feature__Card__Body__Description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna,
+                tortor tempus.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
       <div className="Booking__RightSide">
         <div className="Booking__RightSide__TripCard">
-          <h1>HEllo</h1>
+          <img src={TripCartImage} alt="Trip Card Images" />
+          <div className="Booking__RightSide__TripCard__Body">
+            <h1 className="Booking__RightSide__TripCard__Body__Title">
+              Trip To Greece
+            </h1>
+            <div className="Booking__RightSide__TripCard__Body__Description">
+              <span>14-29 June | </span>
+              <span>by Robbin joseph</span>
+            </div>
+            <div className="Booking__RightSide__TripCard__Body__Icon">
+              <img src={Leaf} alt="Leaf Icon" />
+              <img src={Map} alt="Map Icon" />
+              <img src={Send} alt="Send Icon" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
