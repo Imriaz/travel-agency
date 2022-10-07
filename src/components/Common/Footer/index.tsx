1e4d3bebba069802d9ed709208947a5ee0d67901
Md. Imriaz Uddin <imriaz.uddin@bjitgroup.com>
2022-10-07 17:24:22 +0600

    Footer complete

--- src/components/Common/Footer/index.tsx
+++ src/components/Common/Footer/index.tsx
@@ -9,47 +9,53 @@ import AppleStore from "../../../Assets/images/Apple Store.png";
 
 const Footer = () => {
   return (
-    <div className="Footer">
-      <div className="Footer__Logo">
-        <h1 className="Footer__Logo__Title">Jadoo.</h1>
-        <p className="Footer__Logo__Description">
-          Book your trip in minute, get full Control for much longer.
-        </p>
-      </div>
-      <div className="Footer__NavContent">
-        <div className="Footer__NavContent__Company">
-          <h3>Company</h3>
-          <p>About</p>
-          <p>Careers</p>
-          <p>Mobile</p>
+    <>
+      <div className="Footer">
+        <div className="Footer__Logo">
+          <h1 className="Footer__Logo__Title">Jadoo.</h1>
+          <p className="Footer__Logo__Description">
+            Book your trip in minute, get full Control for much longer.
+          </p>
         </div>
-        <div className="Footer__NavContent__Contact">
-          <h3>Contact</h3>
-          <p>Help/FAQ</p>
-          <p>Press</p>
-          <p>Affilates</p>
+        <div className="Footer__NavContent">
+          <div className="Footer__NavContent__NavRow">
+            <h3 className="Footer__NavContent__NavRow__Title">Company</h3>
+            <p>About</p>
+            <p>Careers</p>
+            <p>Mobile</p>
+          </div>
+          <div className="Footer__NavContent__NavRow">
+            <h3 className="Footer__NavContent__NavRow__Title">Contact</h3>
+            <p>Help/FAQ</p>
+            <p>Press</p>
+            <p>Affilates</p>
+          </div>
+          <div className="Footer__NavContent__NavRow">
+            <h3 className="Footer__NavContent__NavRow__Title">More</h3>
+            <p>Airlinefees</p>
+            <p>Airline</p>
+            <p>Low </p>
+          </div>
         </div>
-        <div className="Footer__NavContent__More">
-          <h3>More</h3>
-          <p>Airlinefees</p>
-          <p>Airline</p>
-          <p>Low fare tips</p>
-        </div>
-      </div>
-      <div className="Footer__Outbound">
-        <div className="Footer__Outbound__Social">
-          <img src={Facebook} alt="Facebook Logo" />
-          <img src={Instagram} alt="Instagram Logo" />
-          <img src={Twitter} alt="Twitter Logo" />
-        </div>
-        <div className="Footer__Outbound__Discover">
-          <h3>Discover our app</h3>
-          <img src={PlayStore} alt="Google Play Store Logo" />
-          <img src={AppleStore} alt="Apple Store Logo" />
+        <div className="Footer__Outbound">
+          <div className="Footer__Outbound__Social">
+            <img src={Facebook} alt="Facebook Logo" />
+            <img src={Instagram} alt="Instagram Logo" />
+            <img src={Twitter} alt="Twitter Logo" />
+          </div>
+          <div className="Footer__Outbound__Discover">
+            <h3 className="Footer__Outbound__Discover__Title">
+              Discover our app
+            </h3>
+            <div className="Footer__Outbound__Discover__Logo">
+              <img src={PlayStore} alt="Google Play Store Logo" />
+              <img src={AppleStore} alt="Apple Store Logo" />
+            </div>
+          </div>
         </div>
       </div>
       <h1 className="Footer__Copyright">All rights reserved@jadoo.co</h1>
-    </div>
+    </>
   );
 };
 
